df77495e2972f0d57d19aca75358a0e46f552197
abarnez <icecapper@hotmail.com>
2020-10-04 18:26:15 -0400

    Upload Project
    uploaded my unity project to github

---
+++ GAME2014_A1_BarnesAlexander/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,40 @@
+﻿/* 
+ Alexander Barnes 101086806
+ GAME2014_A1_BARNES_ALEXANDER
+ BUTTON_MANAGER
+ SCRIPT USED TO CHANGE SCENES AFTER BUTTON ONCLICK EVENT IS CALLED WITH PUBLIC STRING FIELD SO YOU CAN TYPE NAME OF SCENE YOU WANT
+ IN EDITOR, CURRENTLY SET UP FOR ONLY TWO BUTTONS AS NONE OF MY SCENES HAVE MORE THAN 2 BUTTONS
+ LAST MODIFIED 10/4/2020
+ VERSION 1.0.0
+ */
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public Button StartButton;
+    public Button InstructButton;
+    public string scene1;
+    public string scene2;
+ 
+    void Start()
+    {
+        Button btn = StartButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        Button btn2 = InstructButton.GetComponent<Button>();
+        btn2.onClick.AddListener(TaskOnClick2);
+    }
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        SceneManager.LoadScene(scene1);
+    }
+    void TaskOnClick2()
+    {
+        Debug.Log("You have clicked the button!");
+        SceneManager.LoadScene(scene2);
+    }
+
+}
---
+++ GAME2014_A1_BarnesAlexander/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,43 @@
+﻿/* 
+ Alexander Barnes 101086806
+ GAME2014_A1_BARNES_ALEXANDER
+ PLAYER_SCRIPT
+ CURRENTLY NOT WORKING SCRIPT, WAS GOING TO USE TO CHANGE SCENE, EVENT SYSTEM DOESNT SEEM TO RESPOND TO INPUT EVENTS 
+ LAST MODIFIED 10/4/2020
+ VERSION 1.0.0
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("enter pressed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("enter");
+            Debug.Log("enter pressed");
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            print("enter");
+            Debug.Log("enter pressed");
+        }
+    }
+
+    public void OnGUI()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("enter");
+            Debug.Log("enter pressed");
+        }
+    }
+}
